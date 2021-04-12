18776abc25868eb10c10c6b06e869189173c609f
damir224 <damir224@bk.ru>
2021-04-12 10:21:14 +0500

    route panel fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,14 +6,29 @@ import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white',
   },
   pos: {
     display: 'flex',
     justifyItems: 'center',
     position: 'static',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
   },
 }));
 
@@ -24,15 +39,33 @@ export default function Navbar() {
   return (
     <div className={classes.root}>
       <AppBar className={classes.pos}>
-        <Link to='/'>Home</Link>
-        {state.userReducers.user.isAuth ? (
-          <Link to='/logout'>Logout</Link>
-        ) : (
-          <>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Sign up</Link>
-          </>
-        )}
+        <Toolbar>
+          <Typography variant='h6' className={classes.title}>
+            <Link to='/' className={classes.link}>
+              Home
+            </Link>
+          </Typography>
+          <Typography color='inherit'>
+            {state.userReducers.user.isAuth ? (
+              <Link to='/logout' className={classes.link}>
+                Logout
+              </Link>
+            ) : (
+              <>
+                <Link
+                  to='/login'
+                  style={{ marginRight: 10 }}
+                  className={classes.link}
+                >
+                  Login
+                </Link>
+                <Link to='/signup' className={classes.link}>
+                  Sign up
+                </Link>
+              </>
+            )}
+          </Typography>
+        </Toolbar>
       </AppBar>
     </div>
   );

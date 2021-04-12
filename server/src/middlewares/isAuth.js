83a6a0cc5bfa04d197ffe680ee6d407b09dee60e
damir224 <damir224@bk.ru>
2021-04-12 10:00:39 +0500

    deleted server

--- server/src/middlewares/isAuth.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  console.log('file-auth.js req.session.user:', req.session.user);
-  if (!req.session.user) {
-    res.json({ session: false, message: 'not authorize' });
-  } else next();
-};
--- server/src/middlewares/user.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.session.user) res.locals.username = req.session?.user?.name;
-  next();
-};

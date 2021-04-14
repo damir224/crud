e209ee63e62f877d980a221b5d4fd7f2b470b57a
damir224 <damir224@bk.ru>
2021-04-14 12:00:16 +0500

    commit

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,5 +1,29 @@
 // import dotenv from 'dotenv';
-
+// new HtmlWebpackPlugin(
+//   Object.assign(
+//     {},
+//     {
+//       inject: true,
+//       template: paths.appHtml,
+//     },
+//     isEnvProduction
+//       ? {
+//           minify: {
+//             removeComments: true,
+//             collapseWhitespace: true,
+//             removeRedundantAttributes: true,
+//             useShortDoctype: true,
+//             removeEmptyAttributes: true,
+//             removeStyleLinkTypeAttributes: true,
+//             keepClosingSlash: true,
+//             minifyJS: true,
+//             minifyCSS: true,
+//             minifyURLs: true,
+//           },
+//         }
+//       : undefined
+//   )
+// ),
 export default async function uniFetch(...args) {
   // dotenv.config();
   const arr = ['token', 'url', 'method', 'body'];
